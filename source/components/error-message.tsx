6cc54bf8c4c6c2b338bad0306accccb81bacabf9
h4r1z1b4mx <harizibamv@gmail.com>
2025-09-28 00:18:00 +0530

    completed the error message component

--- source/components/error-message.tsx
+++ source/components/error-message.tsx
@@ -1,5 +1,8 @@
 import { memo } from "react";
-
+import {TitledBox, titleStyles} from '@mishieck/ink-titled-box';
+import { useTerminalWidth } from "../hooks/useTerminalWidth.js";
+import { useTheme } from "../hooks/useTheme.js";
+import {Box, Text} from 'ink'
 
 
 export default memo(function ErrorMessage(
@@ -14,9 +17,40 @@ export default memo(function ErrorMessage(
 	}
 ){
 
-	const boxWidth = use
-
+	const boxWidth = useTerminalWidth();
+	const {colors} = useTheme();
 	return (
-
+		<>
+			{ hideBox ?(
+				<Box width={boxWidth} flexDirection="column" marginBottom={1}>
+					<Text color={colors.error}>{message}</Text>
+				</Box>
+			): hideTitle?(
+				<Box
+					borderStyle="round"
+					width={boxWidth}
+					borderColor={colors.error}
+					paddingX={2}
+					paddingY={0}
+					flexDirection="column"
+				>
+					<Text color={colors.error}>{message}</Text>
+				</Box>
+			):(
+				<TitledBox
+					key={colors.primary}
+					borderStyle="round"
+					titles={['Error']}
+					titleStyles={titleStyles.pill}
+					width={boxWidth}
+					borderColor={colors.error}
+					paddingX={2}
+					paddingY={1}
+					flexDirection="column"
+				>
+					<Text color={colors.error}> {message}</Text>
+				</TitledBox>
+			)}
+		</>
 	);
 });
